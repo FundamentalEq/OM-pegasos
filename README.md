1a1f17848261e7d62ea10b165c69c1f3cec8d0b2
fundamentaleq <bloodspain@gamil.com>
2019-05-03 00:54:59 +0530

    upd

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # OM-pegasos
+
+Libraries Required : <br>
+
+SKLearn for accuracy
+
+To run <br>
+
+bash run.sh
---
+++ run.sh
@@ -0,0 +1,3 @@
+ python main.py data/train.txt  data/test.txt 5
+
+ # python main.py train_file test_file batch_size
